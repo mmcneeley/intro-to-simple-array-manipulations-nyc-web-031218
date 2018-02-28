99e86cd4eeab760da1dfe30310ddf6e465fa24a1
Mike McNeeley <mmcneeley@gmail.com>
2018-02-28 23:20:40 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,15 @@
 def using_push(array, stirng)
   array.push(string)
 end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def using_pop_with_args(array,)
+
+end
