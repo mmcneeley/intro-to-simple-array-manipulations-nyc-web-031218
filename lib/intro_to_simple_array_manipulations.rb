4fec68f049e0ece2aa46d156f0b6fc1156bf39ae
Mike McNeeley <mmcneeley@gmail.com>
2018-02-28 23:22:11 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,5 +11,13 @@ def using_pop(array)
 end
 
 def using_pop_with_args(array)
-  array.pop(-2,-1)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+    array.shift(2)
 end
